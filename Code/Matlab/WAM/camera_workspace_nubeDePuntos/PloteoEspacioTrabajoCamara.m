98265f91feeffdfeed21e77b4efa6b60f9fe3c69
Juan María Herrera López <juanma56herrera@gmail.com>
2022-05-22 13:21:59 +0200

    añadida funcion PosCamaraSimulador al script PloteoEspacioTrabajoCamara

--- Code/Matlab/WAM/camera_workspace_nubeDePuntos/PloteoEspacioTrabajoCamara.m
+++ Code/Matlab/WAM/camera_workspace_nubeDePuntos/PloteoEspacioTrabajoCamara.m
@@ -1,11 +1,4 @@
-%% Calculo de aristas para el poliedro
-% addpath('funciones_matlab')
-
-% limiteHorizontal = 40;
-% limiteVerticalBajo = 10;
-% limiteVerticalAlto = 100;
-% limiteLargoBajo = 0.15;
-% limiteLargoAlto = 0.30;
+%% Calculo de poses limite camara
 
 limiteHorizontal = 30;
 limiteVerticalBajo = 15;
@@ -18,6 +11,7 @@ figure;
 for largo = limiteLargoBajo:0.02:limiteLargoAlto
     for alpha = limiteVerticalBajo:10:limiteVerticalAlto
         for beta = -limiteHorizontal:10:limiteHorizontal
+            % calcula posicion de {cam} respecto {fulcro}
             punto = PosCamaraSimulador(largo,beta,alpha);
             plot3(punto(1),punto(2),punto(3),'k.');
             hold on
@@ -25,17 +19,26 @@ for largo = limiteLargoBajo:0.02:limiteLargoAlto
     end
 end
 
-xlabel('x')
-ylabel('y')
-zlabel('z')
+xlabel('x [m]')
+ylabel('y [m]')
+zlabel('z [m]')
 
-% Dibujo de poligonos
-% figure;
-% patch('XData',[p1(1), p2(1), p3(1), p4(1)],'YData',[p1(2), p2(2), p3(2), p4(2)],'ZData',[p1(3), p2(3), p3(3), p4(3)]);
-% hold on
-% patch('XData',[p5(1), p6(1), p7(1), p8(1)],'YData',[p5(2), p6(2), p7(2), p8(2)],'ZData',[p5(3), p6(3), p7(3), p8(3)]);
-% patch('XData',[p1(1), p4(1), p8(1), p5(1)],'YData',[p1(2), p4(2), p8(2), p5(2)],'ZData',[p1(3), p4(3), p8(3), p5(3)]);
-% patch('XData',[p1(1), p2(1), p6(1), p5(1)],'YData',[p1(2), p2(2), p6(2), p5(2)],'ZData',[p1(3), p2(3), p6(3), p5(3)]);
-% patch('XData',[p6(1), p2(1), p3(1), p7(1)],'YData',[p6(2), p2(2), p3(2), p7(2)],'ZData',[p6(3), p2(3), p3(3), p7(3)]);
-% patch('XData',[p8(1), p7(1), p3(1), p4(1)],'YData',[p8(2), p7(2), p3(2), p4(2)],'ZData',[p8(3), p7(3), p3(3), p4(3)]);
+plot3(0,0,0,'k*'); % punto de fulcro
 
+grid on
+
+function pos = PosCamaraSimulador(rho,beta,alpha)
+%Input en metros y en grados
+
+T = eye(4);
+
+% Rotacion ejes fijos en X de alpha grados
+T = rotX(alpha*pi/180)*T;
+% Rotacion eje movil Z de beta grados
+T = T*rotZ(beta*pi/180);
+% Desplazamiento ejes moviles de rhocm en y
+T = T*desp([0 rho 0]);
+
+pos = T(1:3,4);
+
+end
