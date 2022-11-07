aa3fe5edc44bb1bde319cd41ec87fbae8c34558f
Juan María Herrera López <juanma56herrera@gmail.com>
2022-11-07 12:45:46 +0100

    estudio correccion de error teorico

--- Code/Matlab/scripts/error_angulos_camara/pruebasNumericIKSolver.m
+++ Code/Matlab/scripts/error_angulos_camara/pruebasNumericIKSolver.m
@@ -3,6 +3,21 @@
 wamTree = importrobot("mirobot.urdf");
 ik = inverseKinematics('RigidBodyTree',wamTree);
 weights = [0.25 0.25 0.25 1 1 1];
+th(1).JointName = 'wam/base_yaw_joint';
+th(2).JointName = 'wam/shoulder_pitch_joint';
+th(3).JointName = 'wam/shoulder_yaw_joint';
+th(4).JointName = 'wam/elbow_pitch_joint';
+th(5).JointName = 'wam/wrist_yaw_joint';
+th(6).JointName = 'wam/wrist_pitch_joint';
+th(7).JointName = 'wam/palm_yaw_joint';
+
+th(1).JointPosition = 0;
+th(2).JointPosition = 0;
+th(3).JointPosition = 0;
+th(4).JointPosition = 0;
+th(5).JointPosition = 0;
+th(6).JointPosition = 0;
+th(7).JointPosition = 0;
 %%
 transform = getTransform(wamTree,th,'wam/shoulder_yaw_link');
 altura = transform(3,4);
--- Code/Matlab/scripts/error_angulos_camara/pruebasTeoricas.m
+++ Code/Matlab/scripts/error_angulos_camara/pruebasTeoricas.m
@@ -13,43 +13,67 @@ robotTfulcro = [ -1  0 0  0.583;
 
 % DH1 parameters Barrett WAM
 
-    a1 = 0;         
-    a2 = 0;         
-    a3 = 0.045;     
-    a4 = -0.045;    
-    a5 = 0;
-    a6 = 0;
-    a7 = 0;
-    
-    alpha1 = -pi/2;
-    alpha2 = pi/2;
-    alpha3 = -pi/2;
-    alpha4 = pi/2;
-    alpha5 = -pi/2;
-    alpha6 = pi/2;
-    alpha7 = 0;
-    
-    d1 = 0;
-    d2 = 0;
-    d3 = 0.55;
-    d4 = 0;
-    d5 = 0.3;
-    d6 = 0;
-    d7 = 0.06;
+a1 = 0;         
+a2 = 0;         
+a3 = 0.045;     
+a4 = -0.045;    
+a5 = 0;
+a6 = 0;
+a7 = 0;
 
-    ToolLength = d7 + 0.11;
+alpha1 = -pi/2;
+alpha2 = pi/2;
+alpha3 = -pi/2;
+alpha4 = pi/2;
+alpha5 = -pi/2;
+alpha6 = pi/2;
+alpha7 = 0;
+
+d1 = 0;
+d2 = 0;
+d3 = 0.55;
+d4 = 0;
+d5 = 0.3;
+d6 = 0;
+d7 = 0.06;
+
+ToolLength = d7 + 0.11;
          
 error_rho = [];
 error_alpha = [];
 error_beta = [];
 error_estudiado = [];
-error_posicion = [];
+error_posicion_tras_mci = [];
+error_posicion_tras_numIK = [];
 phis = [];
 phiIns = [];
 media_error_estudiado = [];
 
-% wamTree = importrobot("mirobot.urdf");
-         
+wamTree = importrobot("mirobot.urdf");
+ik = inverseKinematics('RigidBodyTree',wamTree);
+weights = [0.25 0.25 0.25 1 1 1];
+%%
+th(1).JointName = 'wam/base_yaw_joint';
+th(2).JointName = 'wam/shoulder_pitch_joint';
+th(3).JointName = 'wam/shoulder_yaw_joint';
+th(4).JointName = 'wam/elbow_pitch_joint';
+th(5).JointName = 'wam/wrist_yaw_joint';
+th(6).JointName = 'wam/wrist_pitch_joint';
+th(7).JointName = 'wam/palm_yaw_joint';
+
+th(1).JointPosition = 0;
+th(2).JointPosition = 0;
+th(3).JointPosition = 0;
+th(4).JointPosition = 0;
+th(5).JointPosition = 0;
+th(6).JointPosition = 0;
+th(7).JointPosition = 0;
+transform = getTransform(wamTree,th,'wam/shoulder_yaw_link');
+altura = transform(3,4);
+
+
+
+
 % for phiIn = -90:-10:-180
 % % for phiIn = 180:10:270
 % 
@@ -127,7 +151,7 @@ for alpha = 30:5:80
         for rho = 0.14:0.02:0.22
             % Calculo de pose objetivo
             robotTobjetivo = robotTfulcro*PoseCamaraSimulador(rho,beta,alpha)*camTtcp;
-            [thRad, error_estudiado_ahora, phiOut, th, A, UJ, LJ] = mci_wam_antiguo(robotTobjetivo,'O',0.11,0,0,0);
+            [thRad, error_estudiado_ahora, phiOut, semilla, A, UJ, LJ] = mci_wam_antiguo(robotTobjetivo,'O',0.11,0,0,0);
 %             thRad = mci_wam(robotTobjetivo,'O',0.11,0,0,0,0.4);
             
             % Visualizacion
@@ -145,12 +169,21 @@ for alpha = 30:5:80
             % Pose objetivo tras ejecutar mci (la que se envia de verdad)
             robotTobjetivo_tras_mci = MCD_WAM(thRad);
 
+            [configSoln,solnInfo] = ik('wam/wrist_palm_stump_link',desp([0 0 altura])*robotTobjetivo*desp([0 0 -0.11]),weights,semilla);
+            for i = 1:7
+                thRad_tras_numIK(i) = configSoln(i).JointPosition;
+            end
+            robotTobjetivo_tras_numIK =  MCD_WAM(thRad_tras_numIK);
+            error_pos_tras_mci = norm(robotTobjetivo(1:3,4)-robotTobjetivo_tras_mci(1:3,4))
+            error_pos_tras_solver = norm(robotTobjetivo(1:3,4)-robotTobjetivo_tras_numIK(1:3,4))
+            error_posicion_tras_numIK = [error_posicion_tras_numIK, error_pos_tras_solver];
+
             H03 = transfDH1(a1,alpha1,d1,thRad(1)) * ...
                   transfDH1(a2,alpha2,d2,thRad(2)) * ...
                   transfDH1(a3,alpha3,d3,thRad(3)); %* ...
 %                   transfDH1(a4,alpha4,d4,thRad(4));
 
-%             error_estudiado_ahora = norm(H03(1:3,4)-UJ');
+            error_estudiado_ahora = norm(H03(1:3,4)-A');
 %             error_estudiado_ahora = error_estudiado_ahora - norm(H04(1:3,4)-UJ');
 
             AVUJ = UJ'-A';
@@ -171,7 +204,7 @@ for alpha = 30:5:80
             error_alpha = [error_alpha, alpha-alpha_tras_mci];
             error_beta = [error_beta, beta-beta_tras_mci];
             error_estudiado = [error_estudiado, error_estudiado_ahora];
-            error_posicion = [error_posicion, error_posicion_ahora];
+            error_posicion_tras_mci = [error_posicion_tras_mci, error_posicion_ahora];
             phis = [phis, rad2deg(phiOut)];
         end
     end
@@ -249,17 +282,19 @@ end
 % 
 % figure; plot([-10:1:10],error_pos_medio);
 
-figure; plot(error_posicion); ylabel("Error de posición (m)"); xlabel('Muestras');
+figure; plot(error_posicion_tras_mci); ylabel("Error de posición tras mci (m)"); xlabel('Muestras');
+figure; plot(error_posicion_tras_numIK); ylabel("Error de posición tras numIK (m)"); xlabel('Muestras');
+
 figure; plot((error_estudiado)); xlabel('Muestras');
 
 % figure; plot(phis); title('Evolución temporal de \phi seleccionada por el algoritmo'); xlabel('Muestras'); ylabel('\phi (º)');
 % figure; plot(error_rho);        title('Error de \rho');      xlabel('Muestras'); ylabel('e_{\rho} [m]');
-% figure; plot(error_alpha);      title('Error de \alpha');    xlabel('Muestras'); ylabel('e_{\alpha} [º]');
-% figure; plot(error_beta);       title('Error de \beta');     xlabel('Muestras'); ylabel('e_{\beta} [º]');
+figure; plot(error_alpha);      title('Error de \alpha');    xlabel('Muestras'); ylabel('e_{\alpha} [º]');
+figure; plot(error_beta);       title('Error de \beta');     xlabel('Muestras'); ylabel('e_{\beta} [º]');
 
 % figure; plot(phiIns,(media_error_estudiado)); title('Media del error estudiado para cada valor de \phi'); xlabel('\phi (º)'); ylabel('Media del error estudiado');
 
-%% Estimacion del tiempo de ejecucion de mci
-robotTobjetivo = robotTfulcro*PoseCamaraSimulador(0.18,0,25)*camTtcp;
-f = @() mci_wam_antiguo_simbolico(robotTobjetivo,'O',0.11,0,0,0);
-timeit(f)
+% %% Estimacion del tiempo de ejecucion de mci
+% robotTobjetivo = robotTfulcro*PoseCamaraSimulador(0.18,0,25)*camTtcp;
+% f = @() mci_wam_antiguo_simbolico(robotTobjetivo,'O',0.11,0,0,0);
+% timeit(f)
